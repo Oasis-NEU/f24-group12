a7dffb148920dd406a0ec9c14840617504ef6dcc
victoriaw2408 <whisnant.v@northeastern.edu>
2024-11-21 23:05:42 -0500

    dark mode implemented

--- App.js
+++ App.js
@@ -1,29 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity, Pressable, Button } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import React, { useState } from 'react'
-import Ionicons from '@expo/vector-icons/Ionicons' ;
+import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import React, { useEffect, useState, useContext } from 'react'
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './pages/HomeScreen';
 import CalendarScreen from './pages/CalendarScreen';
 import UploadScreen from './pages/UploadScreen';
 import AlbumsScreen from './pages/AlbumsScreen';
 import ProfileScreen from './pages/ProfileScreen';
-import MyAgenda from './pages/MyAgenda';
-
 import AppNavigator from './pages/AppNavigator';
+import { EventRegister } from 'react-native-event-listeners'
+import theme from './theme/theme';
+import themeContext from './theme/themeContext';
+import { background } from '@cloudinary/url-gen/qualifiers/focusOn';
 const Tab = createBottomTabNavigator();
 
 // function for button to change when being pressed
 const ColorChangingButton = ({ defaultIconName, pressedIconName, iconSize = 40, onPress }) => {
-  const [iconName, setIconName] = useState(defaultIconName);  
+  const [iconName, setIconName] = useState(defaultIconName);
+  const theme = useContext(themeContext);
+
 
   const pressed = () => {
-    setIconName(pressedIconName);  
+    setIconName(pressedIconName);
   };
 
   const unPressed = () => {
-    setIconName(defaultIconName);  
+    setIconName(defaultIconName);
   };
 
   return (
@@ -32,16 +36,18 @@ const ColorChangingButton = ({ defaultIconName, pressedIconName, iconSize = 40, 
       onPressOut={unPressed}
       onPress={onPress}
     >
-      <Ionicons name={iconName} size={iconSize} color="black" />
+      <Ionicons name={iconName} size={iconSize} color={theme.color} />
     </Pressable>
   );
 };
 
 function CustomHeader({ navigation }) {
+  const theme = useContext(themeContext);
+
   return (
-    <View style={styles.header}>
-      <Text style={styles.title}>Fitted</Text>
-      <ColorChangingButton
+    <View style={[styles.header, {backgroundColor: theme}]}>
+      <Text style={[styles.title, {color: theme.color}]}>Fitted.</Text>
+      <ColorChangingButton 
         defaultIconName="dice-outline"
         pressedIconName="dice"
         onPress={() => console.log('Shuffle button pressed')}
@@ -51,64 +57,76 @@ function CustomHeader({ navigation }) {
 }
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(false)
+
+  useEffect(() => {
+    const listener = EventRegister.addEventListener('ChangeTheme', (data) => {
+      setDarkMode(data)
+    })
+    return () => {
+      EventRegister.removeAllListeners(listener)
+    }
+  }, [darkMode])
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+    <themeContext.Provider value={darkMode === true ? theme.dark : theme.light}>
+      <NavigationContainer theme={darkMode === true ? DarkTheme : DefaultTheme}>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
 
-            if (route.name === 'Home') {
-              iconName = focused
-                ? "shirt"
-                : "shirt-outline";
-            }
-            if (route.name === 'Calendar') {
-              iconName = focused 
-              ? "calendar-clear" 
-              : "calendar-clear-outline";
-            }
-            if (route.name === 'Upload') {
-              iconName = focused 
-              ? "add-circle" 
-              : "add-circle-outline";
-            }
-            if (route.name === 'Albums') {
-              iconName = focused 
-              ? "albums" 
-              : "albums-outline";
-            }
-            if (route.name === 'Profile') {
-              iconName = focused 
-              ? "person" 
-              : "person-outline";
-            }
+              if (route.name === 'Home') {
+                iconName = focused
+                  ? "shirt"
+                  : "shirt-outline";
+              }
+              if (route.name === 'Calendar') {
+                iconName = focused
+                  ? "calendar-clear"
+                  : "calendar-clear-outline";
+              }
+              if (route.name === 'Upload') {
+                iconName = focused
+                  ? "add-circle"
+                  : "add-circle-outline";
+              }
+              if (route.name === 'Albums') {
+                iconName = focused
+                  ? "albums"
+                  : "albums-outline";
+              }
+              if (route.name === 'Profile') {
+                iconName = focused
+                  ? "person"
+                  : "person-outline";
+              }
 
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: "#800f2f",
-          tabBarInactiveTintColor: "#6c757d",
-        })}
-      >
-        <Tab.Screen 
-          name="Home" 
-          component={HomeScreen} 
-          options={{
-            header: () => <CustomHeader />,
-            headerStyle: {
-              backgroundColor: 'white', 
-              elevation: 0, 
-              shadowOpacity: 0,
+              return <Ionicons name={iconName} size={size} color={color} />;
             },
-          }} 
-        />
-        <Tab.Screen name="Calendar" component={CalendarScreen} />
-        <Tab.Screen name="Upload" component={UploadScreen} />
-        <Tab.Screen name="Albums" component={AlbumsScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-    
+            tabBarActiveTintColor: "#800f2f",
+            tabBarInactiveTintColor: "#6c757d",
+          })}
+        >
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              header: () => <CustomHeader />,
+              headerStyle: {
+                backgroundColor: 'white',
+                elevation: 0,
+                shadowOpacity: 0,
+              },
+            }}
+          />
+          <Tab.Screen name="Calendar" component={CalendarScreen} />
+          <Tab.Screen name="Upload" component={UploadScreen} />
+          <Tab.Screen name="Albums" component={AppNavigator} />
+          <Tab.Screen name="Profile" component={ProfileScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </themeContext.Provider>
   );
 }
 
@@ -118,81 +136,80 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'flex-end',
-    height: 98, 
+    height: 98,
     paddingHorizontal: 15,
-    backgroundColor: 'white', 
     elevation: 1,
     borderBottomWidth: 0.3,
     borderBottomColor: '#ccc',
   },
   title: {
-    fontSize: 40, 
+    fontSize: 40,
   },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent:'center',
+    justifyContent: 'center',
   },
-  text:{
+  text: {
     position: 'absolute',
-    top:50,
-    left:20,
-    fontSize:45,
+    top: 50,
+    left: 20,
+    fontSize: 45,
   },
   horizontalLine: {
     position: 'absolute',
-    bottom : 90,
-    left:0,
-    right:0,
-    height:1,
+    bottom: 90,
+    left: 0,
+    right: 0,
+    height: 1,
     backgroundColor: 'black',
   },
   topLine: {
     position: 'absolute',
-    top : 105,
-    left:0,
-    right:0,
-    height:1,
+    top: 105,
+    left: 0,
+    right: 0,
+    height: 1,
     backgroundColor: 'black',
   },
   home: {
     position: 'absolute',
-    left:30,
-    right:0,
+    left: 30,
+    right: 0,
     bottom: 35,
 
   },
   calendar: {
     position: 'absolute',
-    left:105,
-    right:0,
+    left: 105,
+    right: 0,
     bottom: 35,
   },
   plus: {
     position: 'absolute',
-    left:180,
-    right:0,
+    left: 180,
+    right: 0,
     bottom: 35,
   },
   album: {
     position: 'absolute',
-    left:255,
-    right:0,
+    left: 255,
+    right: 0,
     bottom: 35,
   },
   profile: {
     position: 'absolute',
-    left:330,
-    right:0,
+    left: 330,
+    right: 0,
     bottom: 35,
 
   },
   dice: {
     position: 'absolute',
-    left:330,
+    left: 330,
     top: 60,
 
   },
-});
 
+});
--- pages/AppNavigator.js
+++ pages/AppNavigator.js
@@ -1,18 +1,20 @@
-// import React from 'react';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import AlbumsScreen from './AlbumsScreen';
+import AlbumDetailScreen from './AlbumDetailScreen';
+import { EventRegister } from 'react-native-event-listeners'
 
-// import CalendarScreen from './CalendarScreen';
-// import MyAgenda from './MyAgenda';
-// const Stack = createStackNavigator();
 
-// export default function AppNavigator() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator initialRouteName='Calendar'>
-//         <Stack.Screen name="Calendar" component={CalendarScreen} />
-//         <Stack.Screen name="MyAgenda" component={MyAgenda} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
+const Stack = createStackNavigator();
+console.log(Stack.Navigator.screens);
+
+export default function AppNavigator() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Albums" component={AlbumsScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="AlbumDetailScreen" component={AlbumDetailScreen} options={{
+          headerTitle: "",}}/>
+      </Stack.Navigator>
+    );
+  }
